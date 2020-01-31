d9fca2b451fe20c96d06e49b356fe66d2f2e1bb3
Meelouth <justtelejka@yandex.ru>
2020-01-31 20:23:13 +0300

    Basic pour system

---
+++ Assets/Scripts/PouringEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PouringEffect : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+
+    private Transform effectTransform;
+    void Start()
+    {
+        effectTransform = transform;
+        lineRenderer = GetComponent<LineRenderer>();
+        
+        SetLineRenderPosition(Vector3.zero);
+    }
+
+    private void SetLineRenderPosition(Vector3 position)
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, position);
+    }
+    
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(effectTransform.position, Vector3.down, out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(effectTransform.position, Vector3.down * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+            SetLineRenderPosition(hit.point);
+        }
+    }
+}
---
+++ Assets/Scripts/PouringOut.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PouringOut : MonoBehaviour
+{
+    [SerializeField] private Transform pourPoint;
+
+    [SerializeField] private GameObject pourEffect;
+
+    [SerializeField] private float angleToPour = 30;
+
+    [SerializeField] private float speed = 1f;
+
+
+    private Transform pouringObjectTransform;
+
+    private GameObject currentPourEffect;
+    void Start()
+    {
+        pouringObjectTransform = transform;
+    }
+    
+    void Update()
+    {
+        if (CalculateAngle() < angleToPour)
+        {
+            PourOut();
+            return;
+        }
+        
+        StopPouring();
+    }
+
+    private void PourOut()
+    {
+        if (!currentPourEffect)
+            currentPourEffect = Instantiate(pourEffect, pourPoint);
+    }
+
+    private void StopPouring()
+    {
+        Destroy(currentPourEffect);
+    }
+
+    private float CalculateAngle()
+    {
+        return pouringObjectTransform.up.y * Mathf.Rad2Deg;
+    }
+}
